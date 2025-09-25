517f410e49578fe020ddd8b1865732b2a618f349
Joey <leejoey0921@gmail.com>
2025-09-26 02:51:23 +0900

    [P9] fix: update static storage config

--- server/config/settings/production.py
+++ server/config/settings/production.py
@@ -8,21 +8,6 @@ ALLOWED_HOSTS = [
     'ec2-43-202-157-112.ap-northeast-2.compute.amazonaws.com',
 ]
 
-# Production MySQL database using RDS (override base.py)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USERNAME'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOSTNAME'),
-        'PORT': os.getenv('DB_PORT', '3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        },
-    }
-}
-
 # AWS S3 Settings for Static and Media Files
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
@@ -36,10 +21,9 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_S3_VERIFY = True
 AWS_QUERYSTRING_AUTH = False
 
-# Static files (CSS, JavaScript, Images) - Override base.py settings for S3
+# Static files (CSS, JavaScript, Images)
 AWS_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-STATIC_ROOT = None  # Not needed when using S3
 STATICFILES_STORAGE = 'config.storages.StaticS3Boto3Storage'
 
 # Media files (separate from static)
