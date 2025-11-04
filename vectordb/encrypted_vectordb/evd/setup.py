da53535571453797fdd8d17790b9e623b52e8fcf
maum <maum@maumui-MacBookPro.local>
2025-11-04 19:12:31 +0900

    fix: update setup.py to support m3

--- vectordb/encrypted_vectordb/evd/setup.py
+++ vectordb/encrypted_vectordb/evd/setup.py
@@ -4,18 +4,20 @@ import sys
 import os
 import subprocess
 import setuptools
+import platform
+import shlex
 
 __version__ = '0.0.1'
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
+        super().__init__(name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
 class CMakeBuild(build_ext):
     def run(self):
         try:
-            out = subprocess.check_output(['cmake', '--version'])
+            subprocess.check_output(['cmake', '--version'])
         except OSError:
             raise RuntimeError("CMake must be installed to build the extension")
 
@@ -24,16 +26,27 @@ class CMakeBuild(build_ext):
 
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+
         cmake_args = [
             f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}',
             f'-DPython3_EXECUTABLE={sys.executable}',
             '-DBUILD_PYTHON=ON',
-            '-DEVD_BUILD_EXAMPLES=OFF'
+            '-DEVD_BUILD_EXAMPLES=OFF',
+            '-DCMAKE_BUILD_TYPE=Release',
         ]
 
+        if platform.system() == "Darwin" and platform.machine() == "arm64":
+            cmake_args += ['-DCPU_FEATURES_ARCH_ARM=ON', '-DCMAKE_OSX_ARCHITECTURES=arm64']
+
+        env_cmake_args = os.environ.get("CMAKE_ARGS", "")
+        if env_cmake_args:
+            cmake_args += shlex.split(env_cmake_args)
+
+        if os.environ.get("HEXL_NO_CPUFEATURES", "") not in ("", "0", "False", "false", "off", "OFF"):
+            cmake_args += ['-DHEXL_USE_CPUFEATURES=OFF']
+
         build_temp = os.path.join(self.build_temp, ext.name)
-        if not os.path.exists(build_temp):
-            os.makedirs(build_temp)
+        os.makedirs(build_temp, exist_ok=True)
 
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=build_temp)
         subprocess.check_call(['cmake', '--build', '.', '--config', 'Release'], cwd=build_temp)
@@ -46,8 +59,8 @@ setup(
     description='Python bindings for EVD library',
     long_description='',
     ext_modules=[CMakeExtension('evd_py', '.')],
-    cmdclass=dict(build_ext=CMakeBuild),
+    cmdclass={'build_ext': CMakeBuild},
     zip_safe=False,
     python_requires='>=3.7',
     classifiers=[],
-) 
+)
