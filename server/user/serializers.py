9f7800d81600eb4384f4cc77f32733f73e50d604
Joey <leejoey0921@gmail.com>
2025-11-26 00:41:58 +0900

    [P83] reset password API

--- server/user/serializers.py
+++ server/user/serializers.py
@@ -1,6 +1,72 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_str
 from .models import User
+
+User = get_user_model()
+
+
+from django.utils import timezone
+from datetime import timedelta
+import random
+import string
+from .models import PasswordResetOTP
+
+
+class PasswordResetRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+    def validate_email(self, value):
+        # We don't verify user existence to prevent enumeration
+        return value
+
+    def save(self):
+        email = self.validated_data["email"]
+        try:
+            user = User.objects.get(email=email)
+            # Generate 6-digit OTP
+            otp = "".join(random.choices(string.digits, k=6))
+            # Set expiration (15 minutes)
+            expires_at = timezone.now() + timedelta(minutes=15)
+
+            PasswordResetOTP.objects.create(user=user, otp=otp, expires_at=expires_at)
+            return user, otp
+        except User.DoesNotExist:
+            return None, None
+
+
+class PasswordResetConfirmSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    otp = serializers.CharField(max_length=6)
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        otp = attrs.get("otp")
+
+        try:
+            user = User.objects.get(email=email)
+            reset_otp = PasswordResetOTP.objects.filter(
+                user=user, otp=otp, expires_at__gt=timezone.now()
+            ).latest("created_at")
+
+            attrs["user"] = user
+            attrs["reset_otp"] = reset_otp
+            return attrs
+        except (User.DoesNotExist, PasswordResetOTP.DoesNotExist):
+            raise serializers.ValidationError("Invalid email or expired OTP")
+
+    def save(self):
+        user = self.validated_data["user"]
+        password = self.validated_data["password"]
+        user.set_password(password)
+        user.save()
+
+        # Delete used OTP
+        self.validated_data["reset_otp"].delete()
+        return user
 
 
 class UserSignupSerializer(serializers.ModelSerializer):
