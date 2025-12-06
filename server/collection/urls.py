30ea4ddf7360779200714a002a22c0d3b24549ec
Joey <leejoey0921@gmail.com>
2025-12-06 16:46:17 +0900

    Implement sleep API

--- server/collection/urls.py
+++ server/collection/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("search/", views.search_collections, name="search_collections"),
     path("query/", views.query_collection, name="query_collection"),
     path("clear/", views.clear_collections, name="clear_collections"),
+    # Debug
+    path("sleep/", views.debug_sleep, name="debug_sleep"),
 ]
